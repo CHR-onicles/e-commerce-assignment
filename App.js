d937e8f6325cd06c035e20ee0aa12f8c14f05e2b
CHR-onicles <tpandivine48@gmail.com>
2021-10-14 10:06:38 +0000

    finished with login screen

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './screens/login';
+import Home from './screens/home';
 
 export default function App() {
+  const mainNavigator = createStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <NavigationContainer>
+        <mainNavigator.Navigator  screenOptions={{headerShown: false}} initialRouteName="Login">
+          <mainNavigator.Screen name="Home" component={Home} />
+          <mainNavigator.Screen name="Login" component={Login} />
+        </mainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text, Image} from 'react-native'
+
+export default function Home(){
+    return (
+        <View style={{flex: 1, backgroundColor: "red"}}>
+            <Text>Testing this thingy</Text>
+        </View>
+    )
+}
